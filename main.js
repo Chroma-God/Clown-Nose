03fc923a72ff1f2cde1347c1b834f5edd5159cf4
Chroma-God <86387797+Chroma-God@users.noreply.github.com>
2022-03-15 18:58:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function modelLoaded() {
 
 function draw() {
    image(video, 0, 0, 300, 300);
-   image(clown_nose, noseX, noseY, 30, 30);
+   image(clown_nose, noseX-15, noseY-15, 30, 30);
 }
 
 function take_snapshot() {
